3af8a0df116f37660f98a586825b8b47d07ed2cb
Felipe Balbi <balbif@gmail.com>
2015-02-11 10:14:09 -0600

    project: read and clean features.txt
    This commit will read features.txt and clean
    the data so that we're left with properly
    named features.
    
    Signed-off-by: Felipe Balbi <balbif@gmail.com>

--- run_analysis.R
+++ run_analysis.R
@@ -8,6 +8,10 @@
 
 ## Quite a few functions are defined here
 
+##################################################################
+##			LIBRARY DEPENDENCY			##
+##################################################################
+library(dplyr)
 
 ##################################################################
 ##			GLOBAL CONSTANTS			##
@@ -54,4 +58,46 @@ download.extract <- function()
 		file.rename(paste(destination.directory, old, sep = "/"),
 			    paste(destination.directory, destination.source, sep = "/"))
 	}
+}
+
+##
+# read.features - read features.txt into a proper data frame
+#
+# This function will read features.txt and normalize the names of the features
+# before returning final result.
+#
+# All transformations were done using dplyr's select() and mutate() in
+# conjunction with sub(), gsub(), and tolower().
+#
+# The list of transformations follows below:
+#	1. make all feature names lower case
+#	2. split the string so that tBody and fBody read as t.body and f.body
+#	respectively
+#	3. replace - with .
+#	4. remove () pairs
+#	5. remaining ( are converted into .
+#	6. remaining ) are removed
+#	7. remove duplicate 'bodybody' so that we're left with 'body'
+#	8. always add a . after each word
+#	9. remove any duplicate period characters there might have been added
+#	after step 8
+##
+read.features <- function()
+{
+	filename <- paste(destination.directory, destination.source,
+			  "features.txt", sep = "/")
+	features <- read.table(filename, sep = " ", header = FALSE)
+	names(features) <- c("id", "feature")
+
+	features <- mutate(features, feature = tolower(feature))
+	features <- mutate(features, feature = sub("^(t|f)", "\\1\\.", feature))
+	features <- mutate(features, feature = gsub("\\-|\\,", ".", feature))
+	features <- mutate(features, feature = gsub("\\(\\)", "", feature))
+	features <- mutate(features, feature = gsub("\\(", ".", feature))
+	features <- mutate(features, feature = gsub("\\)", "", feature))
+	features <- mutate(features, feature = gsub("bodybody", "body", feature))
+	features <- mutate(features, feature = gsub("(body|gravity|gyro|mag|acc|jerk|energy|bands)", "\\1\\.", feature))
+	features <- mutate(features, feature = gsub("\\.\\.", ".", feature))
+
+	features
 }
